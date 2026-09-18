de5d9e742ea9251c76d10029699c0ce2973183d9
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-09-18 15:11:36 +1000

    https://github.com/mP1/walkingkooka/pull/3326 PublicStaticHelperTesting extends PublicClassTesting
    - https://github.com/mP1/walkingkooka/pull/3326
    - PublicStaticHelperTesting extends PublicClassTesting

--- src/test/java/walkingkooka/text/printer/IndentingPrintersTest.java
+++ src/test/java/walkingkooka/text/printer/IndentingPrintersTest.java
@@ -17,7 +17,6 @@
 
 package walkingkooka.text.printer;
 
-import walkingkooka.reflect.JavaVisibility;
 import walkingkooka.reflect.PublicStaticHelperTesting;
 
 import java.lang.reflect.Method;
@@ -32,10 +31,5 @@ final public class IndentingPrintersTest implements PublicStaticHelperTesting<In
     @Override
     public boolean canHavePublicTypes(final Method method) {
         return false;
-    }
-
-    @Override
-    public JavaVisibility typeVisibility() {
-        return JavaVisibility.PUBLIC;
     }
 }
--- src/test/java/walkingkooka/text/printer/PrintersTest.java
+++ src/test/java/walkingkooka/text/printer/PrintersTest.java
@@ -17,7 +17,6 @@
 
 package walkingkooka.text.printer;
 
-import walkingkooka.reflect.JavaVisibility;
 import walkingkooka.reflect.PublicStaticHelperTesting;
 
 import java.lang.reflect.Method;
@@ -32,10 +31,5 @@ final public class PrintersTest implements PublicStaticHelperTesting<Printers> {
     @Override
     public boolean canHavePublicTypes(final Method method) {
         return false;
-    }
-
-    @Override
-    public JavaVisibility typeVisibility() {
-        return JavaVisibility.PUBLIC;
     }
 }
